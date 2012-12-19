4ac315f2cff9ef6fa636b380ee7441c64b09b0e1
Marco Brandizi <brandizi@ebi.ac.uk>
2012-12-19 11:27:59 +0000

    Encoding problems.

--- src/main/java/uk/ac/ebi/fg/core_model/expgraph/ProtocolApplication.java
+++ src/main/java/uk/ac/ebi/fg/core_model/expgraph/ProtocolApplication.java
@@ -88,7 +88,7 @@ public class ProtocolApplication extends Identifiable
   public String toString() 
   {
   	return String.format ( 
-  		"type: %s { id: %d, acc: %s, order: %d, protocol: %s, parameters:\n  %s\n}", 
+  		"type: %s {id: %d, acc: %s, order: %d, protocol: %s, parameters:\n  %s\n}", 
   		this.getClass ().getSimpleName (), this.getId (), this.getOrder (), 
   		this.getProtocol () == null ? null : this.protocol.getAcc () + " (" + this.protocol.getName () + " )",
   		this.getParameterValues ()
